d4bcef2d1fdcbcf17b109329227aa9a9c626bec1
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-08-18 22:37:58 +0700

    Create Header component & put to arrange LandingPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3252DF",
+        gray: {
+          900: "#152C5B",
+        },
+      },
+      fontSize: {
+        logo: "1.625rem",
+      },
+      fontFamily: {
+        body: ["Poppins"],
+      },
+    },
   },
   variants: {
     extend: {},
