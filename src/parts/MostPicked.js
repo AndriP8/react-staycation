91494390cbb999f7353f2e46bffadf5807505c77
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-08-22 14:13:49 +0700

    add key & remaining style component Most picked

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,12 +4,12 @@ import Button from "../elements/Button";
 
 export default function MostPicked(props) {
   return (
-    <section className="container my-16 max-w-full px-8 lg:px-36 md:px-32">
+    <section className="container mt-16 max-w-full px-8 lg:px-36 md:px-32" ref={props.refMostPicked}>
       <h4 className="mb-5 text-2xl text-gray-900 font-medium">Most Picked</h4>
       <div className="grid grid-cols-3 grid-rows-2 gap-7">
         {props.data.map((item, index) => {
           return (
-            <div className={`${index === 0 ? " row-span-2 h-460" : "row-span-1 h-215"}`}>
+            <div key={`mostpicked-${index}`} className={`${index === 0 ? " row-span-2 h-460" : "row-span-1 h-215"}`}>
               <div className="card-featured h-full relative cursor-pointer">
                 <div className="tag z-10 text-sm bg-tagPrice py-1 px-6 absolute top-0 right-0 text-white h-7 rounded-tr-2xl rounded-bl-2xl">
                   ${item.price}
