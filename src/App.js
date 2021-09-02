60ff2333159a5c38a878d84050d2f4f7b720c667
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-09-02 16:37:25 +0700

    Finish Component Details page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import DetailsPage from "./pages/DetailsPage";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
       </Router>
     </div>
   );
---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+
+import Header from "../parts/Header";
+import PageDetailTitle from "../parts/PageDetailTitle";
+import FeaturedImage from "../parts/FeaturedImage";
+import PageDetailDescription from "../parts/PageDetailDescription";
+import BookingForm from "../parts/BookingForm";
+import Testimony from "../parts/Testimony";
+import Category from "../parts/Category";
+import Footer from "../parts/Footer";
+
+import ItemDetails from "../json/itemDetails.json";
+export default class DetailsPage extends Component {
+  componentDidMount() {
+    document.title = "Details Page";
+  }
+  render() {
+    const breadcrumbList = [
+      { pagTitle: "Home", pagehref: "/" },
+      { pagTitle: "House Details", pagehref: "" },
+    ];
+    return (
+      <>
+        <Header {...this.props} />
+        <PageDetailTitle data={ItemDetails} breadcrumb={breadcrumbList} />
+        <FeaturedImage data={ItemDetails.imageUrls} />
+        <div className="container px-8 lg:px-36 md:px-32">
+          <div className="grid grid-cols-12 grid-rows-1">
+            <div className="col-start-1 col-end-8 pr-6">
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
+            </div>
+            <div className="col-start-8 col-end-13">
+              <Fade bottom>
+                <BookingForm itemDetails={ItemDetails} />
+              </Fade>
+            </div>
+          </div>
+        </div>
+        <Category data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+        <Footer />
+      </>
+    );
+  }
+}
